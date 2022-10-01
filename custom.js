060aab14963ca21d4e42c9f84d9c9e949867c355
Mak <mak1312001@gmail.com>
2022-10-01 20:47:33 +0530

    first commit

---
+++ custom.js
@@ -0,0 +1,8 @@
+let know = new Typed('.no',{
+    strings: ["Web Developer ^3500","Web Designer ^3500","Coder ^3500"," Graphic Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+    showCursor: false 
+});
+
